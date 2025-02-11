18b06352fa74595f5547d047ad13a4b548796c8d
Aditya R.F <danggerblack123@gmail.com>
2025-02-11 07:41:46 +0700

    acak dadu

---
+++ main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:acakdadu/gradient_container.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: GradientContainer(
+            Color.fromARGB(255, 46, 19, 63), Color.fromARGB(255, 139, 24, 159)),
+      ),
+    ),
+  );
+}
---
+++ rollerr.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+final randomizer = Random();
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var currentDiceRoll = 2;
+  var currentDiceRoll2 = 2;
+
+  void rollDice() {
+    setState(() {
+      currentDiceRoll = randomizer.nextInt(6) + 1;
+      currentDiceRoll2 = randomizer.nextInt(6) + 1;
+    });
+  }
+
+  @override
+  build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/image/dice-$currentDiceRoll.png',
+          width: 200,
+        ),
+        Image.asset(
+          'assets/image/dice-$currentDiceRoll2.png',
+          width: 200,
+        ),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            padding: const EdgeInsets.only(
+              top: 20,
+            ),
+            foregroundColor: Colors.white,
+            textStyle: TextStyle(fontSize: 28),
+          ),
+          child: const Text('Roll Dice!'),
+        )
+      ],
+    );
+  }
+}
